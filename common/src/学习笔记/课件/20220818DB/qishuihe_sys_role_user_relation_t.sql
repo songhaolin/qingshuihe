53cbbbe053771a33622e3bd02dbb2a0d0f7cdc09
song <546987377@qq.com>
2022-08-30 17:52:44 +0800

    提交学习课件，视频教程为哔哩哔哩啊av号：BV15G4y1e7b3，up主:漆水河

---
+++ common/src/学习笔记/课件/20220818DB/qishuihe_sys_role_user_relation_t.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: qishuihe
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sys_role_user_relation_t`
+--
+
+DROP TABLE IF EXISTS `sys_role_user_relation_t`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sys_role_user_relation_t` (
+  `id` bigint NOT NULL DEFAULT '0' COMMENT '关联Id',
+  `role_id` bigint DEFAULT '0' COMMENT '角色点',
+  `user_id` bigint DEFAULT '0' COMMENT '用户ID',
+  `create_by` varchar(100) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_by` varchar(100) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `enable` int DEFAULT NULL,
+  `tenant` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='系统角色和用户关联表';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_role_user_relation_t`
+--
+
+LOCK TABLES `sys_role_user_relation_t` WRITE;
+/*!40000 ALTER TABLE `sys_role_user_relation_t` DISABLE KEYS */;
+INSERT INTO `sys_role_user_relation_t` VALUES (10001,10001,10001,'qishuihe','2022-07-21 23:13:00',NULL,NULL,NULL,NULL),(10002,10001,1551197898651254784,'qishuihe','2022-07-21 23:13:00',NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `sys_role_user_relation_t` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-08-18  0:37:09
